eed0650707f8df6f7296932a58ae32f7781fce74
Effy Elden <git@effy.is>
2017-02-14 21:34:25 -0500

    Add Slack notification support

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # nationbuilder-tag-watcher
-Script that reports on new tags in a NationBuilder nation. Can be automated as a cronjob.
+Script that reports on new tags in a NationBuilder nation, via email or slack notification. Can be automated as a cronjob.
 
 ## Usage
+
+### Email reports
 ```
 export NATION=nation_slug
 export API_KEY=valid_api_token
+export MODE=email
 export TO=to@emailaddress.com
 export FROM=from@emailaddress.com
 ruby tagwatcher.rb
 ```
 
+### Slack alerts
 
+You will need a (Slack webhook URL)[https://api.slack.com/services/new/incoming-webhook].
+
+```
+export NATION=nation_slug
+export API_KEY=valid_api_token
+export MODE=slack
+export WEBHOOK_URL=a_slack_webhook_url
+export CHANNEL=nationbuilderalerts
+export USERNAME=NationBot
+ruby tagwatcher.rb
+```
+
+### Multiple nations or configurations
+
+If you would like to easily run multiple instances of the tag watcher, you can use a bash script to set the environment variables before each run.
+
+For simplicity, one option is to create .env.nationslug files in your tag watcher folder, and use the bash `source` command.
--- tagwatcher.rb
+++ tagwatcher.rb
@@ -3,15 +3,18 @@
 require 'mail'
 require 'json'
 require 'nationbuilder'
+require 'slack-notifier'
 
 def get_old_tags(file)
   Dir.mkdir('cache') unless File.exists?('cache')
+  Dir.mkdir('cache/email') unless File.exists?('cache/email')
+  Dir.mkdir('cache/slack') unless File.exists?('cache/slack')
   if File.exist?(file)
     return JSON.parse(File.open(file, 'r').read)
   else 
     p file + " does not exist. Creating cache. No report will be sent."
     file = File.new(file, 'w')
-    file.puts(Array.new)
+    file.puts(Array.new.to_json)
     return Array.new
   end
 end
@@ -40,6 +43,23 @@ def get_tag_list(nation, api_key)
   end
 
   return tags
+end
+
+def get_tagged_people(nation, api_key, tag)
+  client = NationBuilder::Client.new(nation, api_key)
+  people_tagged = client.call(:people_tags, :people, tag: URI.encode(tag), limit: 11)
+
+  people = Array.new
+
+  people_tagged["results"].each do |person|
+    people.push({
+      first_name: person["first_name"],
+      last_name: person["last_name"],
+      id: person["id"]
+    })
+  end
+
+  return people
 end
 
 def send_tag_report(nation, new_tags, from, to)
@@ -74,19 +94,84 @@ def send_tag_report(nation, new_tags, from, to)
   p "Tag report sent."
 end
 
+def send_tag_alert(nation, api_key, new_tags, webhook_url, channel, username)
+  notifier = Slack::Notifier.new webhook_url do
+    defaults channel: channel,
+             username: username
+  end
+
+  tags = Array.new
+
+
+  new_tags.each do |tag|
+    tagged_people = get_tagged_people(nation, api_key, tag)
+    people = Array.new
+    tagged_people.each do |person|
+      people.push("<https://" + nation + ".nationbuilder.com/admin/signups/" + person[:id].to_s + "|" + person[:first_name] + " " + person[:last_name] + ">")
+    end
+    tags.push({
+      fallback: '• ' + tag,
+      title: 'Tag',
+      text: tag,
+      color: "%06x" % (rand * 0xffffff),
+      fields: [
+        {
+          title: 'Tagged people',
+          value: people.join(", ")
+        },
+        {
+          title: 'More than 10 people tagged?',
+          value: (tagged_people.length > 10 ? 'Yes' : 'No')
+        }
+      ]
+    })
+
+    notifier.post text: "New tag(s)", attachments: tags
+  end
+
+
+  p "Tag alert sent."
+
+end
+
 if __FILE__ == $0
 
   NATION = ENV["NATION"]
   API_KEY = ENV["API_KEY"]
-  TO = ENV["TO"]
 
-  if NATION.nil? || API_KEY.nil? || TO.nil?
-    p "Usage: NATION=nation_slug API_KEY=api_key TO=to_email_address FROM=from_email_address"
+  if NATION.nil? || API_KEY.nil?
+    p "Invalid nation credentials. Usage: NATION=nation_slug API_KEY=api_key"
     exit 1
   end
 
-  FROM = ENV["FROM"] ? ENV["FROM"] : ENV["TO"]
-  FILE = ENV["FILE"] ? ENV["FILE"] : 'cache/' + NATION + '.json'
+  MODE = ENV["MODE"]
+
+  if MODE.nil?
+    p "No mode specified. Usage: MODE=email|slack"
+    exit 1
+  elsif MODE != 'email' && MODE != 'slack'
+    p "Invalid mode specified. Valid options: email, slack"
+    exit 1
+  end
+
+  if MODE == 'email'
+    TO = ENV["TO"]
+    FROM = ENV["FROM"] ? ENV["FROM"] : ENV["TO"]
+    if TO.nil?
+      p "Invalid email configuration. Usage: TO=to_email_address (FROM=from_email_address)"
+      exit 1
+    end
+  elsif MODE == 'slack'
+    WEBHOOK_URL = ENV["WEBHOOK_URL"]
+    CHANNEL = ENV["CHANNEL"]
+    USERNAME = ENV["USERNAME"] || "NationBuilder Tag Alerts"
+    if WEBHOOK_URL.nil? || CHANNEL.nil?
+      p "Invalid Slack configuration. Usage: WEBHOOK_URL=slack_webhook_url CHANNEL=channel_name (USERNAME=bot_username)"
+      exit 1
+    end
+  end
+
+  FILE = ENV["FILE"] ? ENV["FILE"] : 'cache/' + MODE + '/' + NATION + '.json'
 
   p "Generating report of new tags on the " + NATION + " nation."
 
@@ -98,7 +183,11 @@ if __FILE__ == $0
 
   if new_tags.any? && new_tags != all_tags
     p new_tags.length.to_s + " new tags."
-    send_tag_report(NATION, new_tags, FROM, TO)
+    if MODE == 'email'
+      send_tag_report(NATION, new_tags, FROM, TO)
+    elsif MODE == 'slack'
+      send_tag_alert(NATION, API_KEY, new_tags, WEBHOOK_URL, CHANNEL, USERNAME)
+    end
   else 
     p "No new tags"
   end
